8790be0e2666dfa65f6dc8e1c842e89120c277a0
Peter Rosemann <dkdndes@gmail.com>
2015-01-25 20:35:17 +0100

    updated forms

--- webservicedoc/forms.py
+++ webservicedoc/forms.py
@@ -15,13 +15,14 @@ class WebservicedocAdminForm(forms.ModelForm):
     """
     class Meta:
         model = Webservicedoc
-        def clean_dwml_raw(self):
-        # Custom WADL validation
-            dwml_raw = self.cleaned_data['dwml_raw']
-            try:
-	        this_dwml = dwml.CreateFromDocument(dwml_raw)
-	    except pyxb.UnrecognizedContentError as e:
-	        raise forms.ValidationError("Error validating response: %s" % e.details())
-	    except Exception, e:
-	        raise forms.ValidationError("Unknown validation error: %s" % e)
-            return dwml_raw
+
+    def clean_dwml_raw(self):
+    # Custom WADL validation
+        dwml_raw = self.cleaned_data['dwml_raw']
+        try:
+            this_dwml = dwml.CreateFromDocument(dwml_raw)
+        except pyxb.UnrecognizedContentError as e:
+            raise forms.ValidationError("Error validating response: %s" % e.details())
+	except Exception, e:
+	    raise forms.ValidationError("Unknown validation error: %s" % e)
+        return dwml_raw
