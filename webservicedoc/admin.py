51a66a008a3750fc036fa363659a2c6838c52f9a
Peter Rosemann <dkdndes@gmail.com>
2015-01-25 17:57:47 +0100

    admin

--- webservicedoc/admin.py
+++ webservicedoc/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from forms import MyWebservicedocAdminForm
 
-# Register your models here.
+"""
+Customize Admin
+"""
+class WebservicedocAdmin(admin.ModelAdmin):
+    form = MyWebservicedocAdminForm
+    save_as = True
+    # lots more unrelated custom admin such as field definitions
